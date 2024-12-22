05423e68402a671d43feacfe54aee0cff9dc3598
nampv-itomo <nampv.itomo@gmail.com>
2024-12-22 11:02:35 +0700

    initialize React app with Electron setup and basic configurations

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "app/preload.js"), // Tùy chọn
+      nodeIntegration: true, // Để sử dụng các API của Node.js
+    },
+  });
+
+  // Load ứng dụng React đã build
+  const appUrl = `file://${path.join(__dirname, "/build/index.html")}`;
+  mainWindow.loadURL(appUrl);
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
