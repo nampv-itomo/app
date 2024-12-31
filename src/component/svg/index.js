2a528b96b15cdd10efd7dccef5e9306c2b2763bf
nampv-itomo <nampv.itomo@gmail.com>
2024-12-31 13:11:17 +0700

    refactor: adjust CSS styles for layout improvements, add video background to MusicPlayer, and update icon colors

--- src/component/svg/index.js
+++ src/component/svg/index.js
@@ -21,11 +21,11 @@ export const shuffleSvg = (
 export const playSvg = (
   <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
     <path
-      fill="#008211"
+      fill="#e1ebf9"
       d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8s8-3.59 8-8s-3.59-8-8-8m-1 12H9V8h2zm4 0h-2V8h2z"
       opacity="0.3"
     />
-    <path fill="#008211" d="M13 8h2v8h-2zM9 8h2v8H9z" />
+    <path fill="#e1ebf9" d="M13 8h2v8h-2zM9 8h2v8H9z" />
     <path
       fill="#008211"
       d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8"
@@ -36,12 +36,12 @@ export const playSvg = (
 export const pauseSvg = (
   <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
     <path
-      fill="#008211"
+      fill="#e1ebf9"
       d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8s8-3.59 8-8s-3.59-8-8-8M9.5 16.5v-9l7 4.5z"
       opacity="0.3"
     />
     <path
-      fill="#008211"
+      fill="#e1ebf9"
       d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8"
     />
     <path fill="#008211" d="m9.5 16.5l7-4.5l-7-4.5z" />
--- src/pages/MusicPlayer.js
+++ src/pages/MusicPlayer.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import "../access/css/Music.css";
 import MusicApp from "../component/components/App";
+import videoBackground from "../access/photos/NỀN CÁC BÀI HÁT.mp4";
 
 const MusicPlayer = () => {
   const navigator = useNavigate();
@@ -11,16 +12,22 @@ const MusicPlayer = () => {
   };
 
   return (
-    <div className="Container">
-      <button className="back-btn" type="button" onClick={handleGo}>
-        Quay lại
-      </button>
-      <div className="MusicPlayer">
-        <div>
-          <MusicApp />
+    <>
+      <video className="video-background-music" autoPlay muted loop>
+        <source src={videoBackground} type="video/mp4" />
+        Trình duyệt của bạn không hỗ trợ video.
+      </video>
+      <div className="Container">
+        <button className="back-btn" type="button" onClick={handleGo}>
+          Quay lại
+        </button>
+        <div className="MusicPlayer">
+          <div>
+            <MusicApp />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
